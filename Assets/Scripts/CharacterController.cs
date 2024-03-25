dfda235bc95339d4d6e3357d5fbc4016644e6ab0
Osokin Vladislav <vladislav.osokin.2005@mail.ru>
2024-03-25 23:15:52 +0300

    feat: added tilemap and wizard

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -11,7 +11,9 @@ public class CharacterController : MonoBehaviour
     private Rigidbody2D rigidbody2D;
     [SerializeField] private float speed = 4f;
     Vector2 motionVector;
+    public Vector2 lastMotionVector;
     Animator animator;
+    public bool moving;
     void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -20,12 +22,25 @@ public class CharacterController : MonoBehaviour
 
     private void Update()
     {
-         motionVector = new Vector2(
-             Input.GetAxisRaw("Horizontal"),
-             Input.GetAxisRaw("Vertical")
-             );
-         animator.SetFloat("horizontal", motionVector.x);
-         animator.SetFloat("vertical", motionVector.y);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        motionVector = new Vector2(
+            horizontal,
+            vertical
+             ).normalized;
+        
+         animator.SetFloat("horizontal", horizontal);
+         animator.SetFloat("vertical", horizontal);
+         moving = horizontal != 0 || vertical != 0;
+         if (horizontal != 0 || vertical != 0)
+         {
+             lastMotionVector = new Vector2(
+                 horizontal,
+                 vertical
+                 ).normalized;
+             animator.SetFloat("lastHorizontal", horizontal);
+                animator.SetFloat("lastVertical", vertical);
+         }
     }
 
     void FixedUpdate()
